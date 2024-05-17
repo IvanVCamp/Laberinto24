0b0d750e77a52c9f4d9626129e2f68413d587afc
IvanVCamp <ivanveca2003@gmail.com>
2024-05-17 18:52:40 +0200

    Formas especificadas...

---
+++ ElementoMapa/Container/Container.py
@@ -0,0 +1,65 @@
+from ElementoMapa.ElementoMapa import ElementoMapa
+import random
+
+class Contenedor (ElementoMapa):
+
+    def __init__(self, n):
+        super().__init__()
+        self.ref = n
+        self.objChildren = []
+        self.form = None
+        self.alcance = None
+
+    def recorrer(self, order):
+        order(self)
+        for ch in self.objChildren:
+            ch.recorrer(order)
+        self.form.recorrer(order)
+
+
+    def estimarPosicion(self):
+        self.form.estimarPosicion()
+
+    def getPunto(self):
+        return self.form.point
+    
+    def setPunto(self,punto):
+        self.form.point = punto
+
+    def getAlcance(self):
+        return self.form.alcance
+    
+    def setExtent(self, alc):
+        self.form.alcance = alc
+
+    def obtenerComandos(self, obj):
+        
+        juegoCommands=[]
+        juegoCommands.extend(self.commands)
+
+        for hijo in self.objChildren:
+            juegoCommands.extend(hijo.getCommands(obj))
+
+        juegoCommands.extend(self.form.getCommands(obj))
+        
+        return juegoCommands
+
+    def addChild(self, obj):
+        obj.parent = self
+        self.objChildren.append(obj)
+
+    def putElementOn(self, obj1, obj2):
+        self.form.putElementOn(obj1, obj2)
+    
+    def notifySubs(self):
+        pass
+
+    def addOr(self, orientacion):
+        self.form.addOr(orientacion)
+
+    def getRandOri(self):
+        reference = random.randint(0,len(self.form.orientaciones)-1)
+        return self.form.orientaciones[reference]
+
+    def getChildren(self):
+        return self.objChildren
