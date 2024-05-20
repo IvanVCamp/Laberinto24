b30002bc03c65c9c93257fb420d0c48c11bdaae3
IvanVCamp <ivanveca2003@gmail.com>
2024-05-20 23:41:45 +0200

    Meto un arma que se me ha ocurrido.

---
+++ Artefactos/Katana.py
@@ -0,0 +1,43 @@
+from Artefactos.Artefacto import Artefacto
+from Command.Usar import Usar
+from Command.Soltar import Soltar
+
+
+class Espada(Artefacto):
+
+    def __init__(self):
+        super().__init__()
+    
+    def esKatana(self):
+        return True
+
+    def aceptar(self, vst):
+        print("Visitar katana")
+        vst.visitarEspada(self)
+    
+    def usar(self, o):
+        o.setEspada(self)
+        o.mochila.usado(self)
+
+        for c in self.comandos:
+            if c.esSoltar():
+                self.deleteCommand(c)
+        for c in self.comandos:
+            if c.esUsar():
+                self.deleteCommand(c)
+    
+    def desequipar(self,ente):
+        ente.setEspada(None)
+        ente.mochila.addArtefacto(self)
+        
+        firstAction = Usar()
+        firstAction.receptor= self
+
+        secondAction = Soltar()
+        secondAction.receptor = self
+
+        self.addCommand(firstAction)
+        self.addCommand(secondAction)
+
+    def __str__(self):
+        return "Katana REF nº: " +str(self.num)
